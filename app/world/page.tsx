320dad6d30225feebabcf1f98ead0ab50499b9b6
Zachary Auker <zachary.auker@gmail.com>
2026-07-05 17:23:21 -0400

    feat: add /world route + World nav item

---
+++ app/world/page.tsx
@@ -0,0 +1,9 @@
+import { WorldMapViewer } from "@/components/maps/WorldMapViewer";
+
+export default function WorldPage() {
+  return (
+    <div className="h-[calc(100vh-3rem)]">
+      <WorldMapViewer />
+    </div>
+  );
+}
